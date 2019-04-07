ce018a6dd3916f2be38b75bbad9408708cdb2827
kv0311 <kvxi98@gmail.com>
2019-04-07 10:25:49 +0700

    call api get city and country and set weather

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import React from 'react';
+import Titles from './components/Title'
+import Form from './components/Form'
+import Weather from './components/Weather'
+const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
+    }
+    this.getWeather.bind(this)
+  }
+  getWeather= (e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault()
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`)
+    .then(response =>response.json())
+    .then(json=>{
+      if(city && country){
+        this.setState({
+          temperature: json.main.temp,
+          city: json.name,
+          country: json.sys.country,
+          humidity: json.main.humidity,
+          description: json.weather[0].description,
+          error: ""
+      })}});
+  }
+  render(){
+    return(
+      
+      <div>
+        <Titles />
+        <Form loadWeather={this.getWeather}></Form>
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error} />
       </div>
-    );
+   )
   }
 }
-
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Titles = (props) => {
+		return(
+			<div>
+				<h1>Weather App</h1>
+				<div>You can find weather in anywhere</div>
+			</div>
+			);  
+}
+export default Titles;
